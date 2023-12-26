bdb2768a33dfb3c28c77707311d5dcfdb5944e4d
aschimmichanga <ashnasrivastava1@gmail.com>
2023-12-26 03:02:51 -0500

    bubble animation polish

--- src/components/sections/Experience/Experience.tsx
+++ src/components/sections/Experience/Experience.tsx
@@ -2,8 +2,8 @@ import BubbleStack from "./BubbleStack";
 
 function Experience() {
     return (
-        <div className="h-screen flex items-center justify-start pl-10 md:pl-20">
-            <div className="text-white text-4xl font-space-mono pt-[10px]">
+        <div className="h-screen w-screen flex items-center justify-start pl-10 md:pl-20">
+            <div className="text-white text-4xl font-space-mono pt-[10px] w-1/3">
                 <span>I've bridged gaps between product and design at companies big and small, reaching over </span>
                 <span className="font-bold text-fuchsia-200">1 billion users</span>
             </div>
