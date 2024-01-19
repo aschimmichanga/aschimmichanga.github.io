99b95dcf7b12d269d4ab1e9cc162b44b430243b5
aschimmichanga <ashnasrivastava1@gmail.com>
2024-01-18 23:08:54 -0500

    reviews first try

--- src/components/sections/Reviews/Reviews.tsx
+++ src/components/sections/Reviews/Reviews.tsx
@@ -5,37 +5,44 @@ import { InfiniteMovingCards } from "~/components/ui/infinite-moving-cards";
 import { LampContainer } from "~/components/ui/lamp";
 import SparklesCore from "~/components/ui/sparkles-core";
 
+function InfiniteMovingCardsDemo() {
+    return (
+        <div className="h-screen w-screen flex-col items-center justify-start pt-20 md:pt-20  pl-20 md:pl-20 font-space-mono text-white">
+            <div className="text-4xl">Rave Reviews</div>
+            <div className="h-[40rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
+                <InfiniteMovingCards
+                    items={testimonials}
+                    direction="right"
+                    speed="slow"
+                />
+            </div>
+        </div>
+    );
+}
+
 const testimonials = [
     {
-        quote:
-            "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-        name: "Charles Dickens",
-        title: "A Tale of Two Cities",
+        quote: "Ashna is an excellent software engineer. She has a very solid technical knowledge base, plus she is very open to learning from feedback. She's able to communicate clearly what she is doing, ask when she needs help and autonomously deliver on projects. It's been a pleasure working together",
+        name: "Oriol Torrillas",
+        title: "Founder & CEO, FLxDER",
     },
     {
-        quote:
-            "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-        name: "William Shakespeare",
-        title: "Hamlet",
+        quote: "Having joined us at a critical time as we were approaching a major milestone developing our product, Ashna has been indispensable to have as a frontend developer. Without any experience with our JavaScript framework, Ashna jumped in and learned it quickly on the fly. She'd be an asset to any team.",
+        name: "Scott Englander",
+        title: "Founder & CEO, FLxDER",
     },
     {
-        quote: "All that we see or seem is but a dream within a dream.",
-        name: "Edgar Allan Poe",
-        title: "A Dream Within a Dream",
+        quote: "She is a quick learner, diving into complex architectures and developing new features in that domain helped her to understand several concepts not only the ones we use internally at Linkedin but also general topics that helps her career growth as a software engineer.",
+        name: "Santiago Vanegas",
+        title: "Senior Android Engineer, Linkedin",
     },
     {
-        quote:
-            "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-        name: "Jane Austen",
-        title: "Pride and Prejudice",
-    },
-    {
-        quote:
-            "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-        name: "Herman Melville",
-        title: "Moby-Dick",
+        quote: "Ashna is a top-notch engineer. I was deeply impressed with how quickly she onboarded to our tech stack. Outside of all the technical bits, she was a joy to work with. Ashna is kind, curious, and lowkey very funny.",
+        name: "Noah Rowlett",
+        title: "Senior Android Engineer, Figma",
     },
 ];
+
 
 function Reviews() {
     return (
@@ -43,21 +50,7 @@ function Reviews() {
         //     <div className="text-4xl">Rave Reviews</div>
         // </div>
         <div>
-            <LampContainer>
-                <motion.h1
-                    initial={{ opacity: 0.5, y: 100 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{
-                        delay: 0.3,
-                        duration: 0.8,
-                        ease: "easeInOut",
-                    }}
-                    className="mt-8 py-4 text-white font-space-mono text-center text-6xl font-medium tracking-tight md:text-6xl"
-                >
-                    <div>Rave Reviews</div>
-
-                </motion.h1>
-            </LampContainer>
+            <InfiniteMovingCardsDemo />
         </div>
     );
 }
